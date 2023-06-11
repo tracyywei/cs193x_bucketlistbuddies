7f337e61569eeaf2581286a2f518bdfd2e5bd205
Tracy Wei <tracywei27@gmail.com>
2023-06-11 13:33:46 -0700

    almost done with backend

--- api/index.js
+++ api/index.js
@@ -47,12 +47,28 @@ api.use("/users/:id", async (req, res, next) => {
   next();
 });
 
+// GET /users/:id
+api.get("/users/:id", async (req, res) => {
+  let user = res.locals.user;
+  delete user._id;
+  res.json(user);
+});
+
 // POST /users
 api.post("/users", async (req, res) => {
   let id = req.body.id;
+  let phone = req.body.phone;
   // 400 if the request body is missing an id property, or the id is empty
+  if (!id && !phone) {
+    res.status(400).json({ error: "Missing id and phone number" });
+    return;
+  }
   if (!id) {
     res.status(400).json({ error: "Missing id" });
+    return;
+  }
+  if (!phone) {
+    res.status(400).json({ error: "Missing phone number" });
     return;
   }
   // 400 if the user already exists
@@ -61,10 +77,125 @@ api.post("/users", async (req, res) => {
     res.status(400).json({ error: `${id} already exists` });
     return;
   }
-  let newUser = { "id": id, "name": id, "avatarURL": "images/default.png", "following": [] };
+  let newUser = { "id": id, "name": id, "avatarURL": "images/default.png", "phone": phone, "activities": [] };
   await Users.insertOne(newUser);
   delete newUser._id;
   res.json(newUser);
+});
+
+
+// PATCH /users/:id
+api.patch("/users/:id", async (req, res) => {
+  let id = req.params.id;
+  let name = req.body.name;
+  let avatarURL = req.body.avatarURL;
+  let user = res.locals.user;
+  if (name === "") {
+    user = await Users.updateOne({ id: id }, { $set: { name: id } });
+  }
+  if (name) {
+    user = await Users.updateOne({ id: id }, { $set: { name: name } });
+  }
+  if (avatarURL === "") {
+    user = await Users.updateOne({ id: id }, { $set: { avatarURL: "images/default.png" } });
+  }
+  if (avatarURL) {
+    user = await Users.updateOne({ id: id }, { $set: { avatarURL: avatarURL } });
+  }
+  user = await Users.findOne({ id: id });
+  delete user._id;
+  res.json(user);
+});
+
+// GET /users/:id/myposts
+api.get("/users/:id/myposts", async (req, res) => {
+  let user = res.locals.user;
+  let activities = user.activities;
+  let userPosts = await Posts.find({ text: { $in: activities } }).toArray();
+  res.json({ "posts": userPosts });
+});
+
+// GET /users/:id/feed
+api.get("/users/:id/feed", async (req, res) => {
+  let user = res.locals.user;
+  let activities = user.activities;
+  let allPosts = await Posts.find().toArray();
+  let userFeed = [];
+  for (let post of allPosts) {
+    if (!activities.includes(post.text)) {
+      userFeed.push(post);
+    }
+  }
+  res.json({ "posts": userFeed });
+});
+
+// POST /users/:id/posts
+api.post("/users/:id/posts", async (req, res) => {
+  let text = req.body.text;
+  // 400 if the request body is missing a text property, or the text is empty
+  if (!text) {
+    res.status(400).json({ error: "Missing text" });
+    return;
+  }
+  let user = res.locals.user;
+  // make a new post
+  await Posts.insertOne({ userId: user.id, time: new Date(), text: text });
+  // add to current bucket list
+  let bucketList = user.activities;
+  bucketList.push(text);
+  await Users.updateOne({ id: user.id }, { $set: { activities: bucketList } });
+  res.json({ success: true });
+});
+
+
+// POST /users/:id/add
+api.post("/users/:id/add", async (req, res) => {
+  let user = res.locals.user;
+  let target = req.query.target;
+  // 400 if the query string is missing a target property, or the target is empty
+  if (!target) {
+    res.status(400).json({ error: "Missing target" });
+    return;
+  }
+  let targetPost = await Posts.find({ text: target });
+  // 404 if user target does not exist
+  if (!targetPost) {
+    res.status(404).json({ error: `No post of ${target}` });
+    return;
+  }
+  let bucketList = user.activities;
+  let index = bucketList.indexOf(target);
+  // 400 if the user is already following the target
+  if (index > -1) {
+    res.status(400).json({ error: `${user.id} has already added ${target} to their bucket list` });
+    return;
+  }
+  // add item to bucket list
+  bucketList.push(target);
+  await Users.updateOne({ id: user.id }, { $set: { activities: bucketList } });
+  res.json({ success: true });
+});
+
+// DELETE /users/:id/add
+api.delete("/users/:id/add", async (req, res) => {
+  let user = res.locals.user;
+  let target = req.query.target;
+  // 400 if the query string is missing a target property, or the target is empty
+  if (!target) {
+    res.status(400).json({ error: "Missing target" });
+    return;
+  }
+  let bucketList = user.activities;
+  let index = bucketList.indexOf(target);
+  // 400 if the target item isn't on the bucket list of the requesting user
+  if (index < 0) {
+    res.status(400).json({ error: `${user.id} does have ${target} on their bucket list` });
+    return;
+  }
+  // deleting the activity
+  bucketList.splice(index, 1);
+  await Users.updateOne({ id: user.id }, { $set: { activities: bucketList } });
+  res.json({ success: true });
 });
 
 
